610ee870b01a4caa49008ab2f4a0c280e9e13337
Danny Brown <csengineer13@gmail.com>
2016-09-17 14:54:54 -0400

    Configure support for XML Comments

--- ImageService/App_Start/SwaggerConfig.cs
+++ ImageService/App_Start/SwaggerConfig.cs
@@ -97,7 +97,7 @@ namespace ImageService
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -241,6 +241,11 @@ namespace ImageService
                         //
                         //c.EnableApiKeySupport("apiKey", "header");
                     });
+        }
+
+        protected static string GetXmlCommentsPath()
+        {
+            return System.String.Format(@"{0}\bin\WebApiSwagger.XML", System.AppDomain.CurrentDomain.BaseDirectory);
         }
     }
 }
